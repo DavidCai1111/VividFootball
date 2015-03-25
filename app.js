9941e4befe8ebabe107a105060ea867b1153019b
DavidCai <376462191@qq.com>
2015-03-25 14:32:13 +0800

    引入newrelic监控

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('newrelic');
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
--- config/mailConfig.js
+++ config/mailConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     account:{
-        username:"?@163.com",
-        password:"?",
+        username:"kc1234569@163.com",
+        password:"qqqaaa",
         host: 'smtp.163.com'
     }
 };
