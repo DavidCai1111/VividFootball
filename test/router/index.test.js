f21f3b8a493de26a41062e689e676bb8fbebd60c
DavidCai <376462191@qq.com>
2015-03-26 15:25:25 +0800

    test

---
+++ test/router/index.test.js
@@ -0,0 +1,9 @@
+var app = require('../../app');
+var request = require('supertest')(app);
+
+describe('test/router/index.test.js',function(){
+    it('请求"/",应取得200状态码',function(done){
+        request.get('/')
+            .expect(200,done);
+    });
+});
