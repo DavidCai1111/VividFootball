a6ff527a71029d4dbadb789e22a4b1bdef30def8
DavidCai <376462191@qq.com>
2015-03-26 19:04:47 +0800

    test

--- config/mailConfig.js
+++ config/mailConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     account:{
-        username:"?@163.com",
-        password:"?",
+        username:"? @163.com",
+        password:"? ",
         host: 'smtp.163.com'
     }
 };
