fcb6533440f885197cf6fa34b2bcc69617ae44a9
DavidCai <376462191@qq.com>
2015-04-14 12:17:13 +0800

    数据缓存

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var rankService = require('../service/rankService');
 var scorerService = require('../service/scorerService');
 var Eventproxy = require('eventproxy');
+var cache = require('memory-cache');
 
 /* 首页默认获取英超排行信息 */
 router.get('/', function(req, res, next) {
@@ -11,6 +12,9 @@ router.get('/', function(req, res, next) {
     var eventproxy = new Eventproxy();
 
     eventproxy.all('rankGet','scorerGet',function(rankGet,scorerGet){
+        //对数据进行缓存，过期时间为5分钟
+        cache.put('rankGet',rankGet,1000*60*5);
+        cache.put('scorerGet',scorerGet,1000*60*5);
         //等待获取到排名信息
         res.render('index', {
             title: '重生的积分榜射手榜！',
@@ -35,8 +39,19 @@ router.get('/', function(req, res, next) {
     });
 
     //获取英超排名
-    var rank = rankService.getRank('England',eventproxy);
-    var scorers = scorerService.getScorer('England',eventproxy);
+    if(cache.get('rankGet') === null){
+        rank = rankService.getRank('England',eventproxy);
+    }else{
+        rank = cache.get('rankGet');
+        eventproxy.emit('rankGet',rank);
+    }
+
+    if(cache.get('scorerGet') === null){
+        scorers = scorerService.getScorer('England',eventproxy);
+    }else{
+        scorers = cache.get('scorerGet');
+        eventproxy.emit('scorerGet',scorers);
+    }
 
 });
 
@@ -46,6 +61,9 @@ router.get('/rank/:league', function(req, res, next) {
     var eventproxy = new Eventproxy();
 
     eventproxy.all('rankGet','scorerGet',function(rankGet,scorerGet){
+        //对数据进行缓存，过期时间为5分钟
+        cache.put('rankGet',rankGet,1000*60*5);
+        cache.put('scorerGet',scorerGet,1000*60*5);
         //等待获取到排名信息
         res.render('index', {
             title: '重生的积分榜射手榜！',
@@ -70,8 +88,19 @@ router.get('/rank/:league', function(req, res, next) {
     });
 
     //获取英超排名
-    var rank = rankService.getRank(req.params.league,eventproxy);
-    var scorers = scorerService.getScorer(req.params.league,eventproxy);
+    if(cache.get('rankGet') === null){
+        rank = rankService.getRank('England',eventproxy);
+    }else{
+        rank = cache.get('rankGet');
+        eventproxy.emit('rankGet',rank);
+    }
+
+    if(cache.get('scorerGet') === null){
+        scorers = scorerService.getScorer('England',eventproxy);
+    }else{
+        scorers = cache.get('scorerGet');
+        eventproxy.emit('scorerGet',scorers);
+    }
 
 });
 
--- routes/mail.js
+++ routes/mail.js
@@ -5,37 +5,37 @@ var rankService = require('../service/rankService');
 var Eventproxy = require('eventproxy');
 var util = require('util');
 
-router.post('/subscribe/rank/:league',function(req,res,next){
+router.post('/subscribe/rank/:league', function (req, res, next) {
 
     var reqBody = req.body;
 
-    if(!isEmail(reqBody.email)){
+    if (!isEmail(reqBody.email)) {
         res.send({
-            result:false
+            result: false
         });
     }
 
     var eventproxy = new Eventproxy();
 
-    eventproxy.all('rankGet',function(rankGet){
+    eventproxy.all('rankGet', function (rankGet) {
 
-        mailService.sendHelloMail(reqBody.email,rankGet,function(err,info){
-            if(err){
-                res.send({
-                    result:false
+        mailService.sendHelloMail(reqBody.email, rankGet, function (err, info) {
+            if (err) {
+                return res.send({
+                    result: false
                 });
             }
-            res.send({
-                result:true,
-                info:info
+            return res.send({
+                result: true,
+                info: info
             });
         });
     });
 
-    var rank = rankService.getRank(req.params.league,eventproxy);
+    var rank = rankService.getRank(req.params.league, eventproxy);
 });
 
-function isEmail(str){
+function isEmail(str) {
     var reg = /^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/;
     return reg.test(str);
 }
